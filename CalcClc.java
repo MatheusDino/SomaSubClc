3e276731ad2ad38ede44243963f9deee15eb0fcc
Matheus Dino <matheusthiago.dino@gmail.com>
2023-08-23 21:00:39 -0300

    Baby steps, mas funciona.

---
+++ CalcClc.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CalczinhaSussa
+{
+	public static void main(String[] args) {
+	    
+	    Scanner scany = new Scanner(System.in);
+	    System.out.println("[Soma] ou [Sub]?");
+	    String operacao = scany.nextLine();
+	    System.out.println("Primeiro número?");
+	    double num1 = scany.nextDouble();
+	    System.out.println("Segundo número?");
+	    double num2 = scany.nextDouble();
+	    if("Soma".equals(operacao)){
+	        System.out.println("Essa operação dá: " + (num1 + num2));
+	    }
+	    if("Sub".equals(operacao)){
+	        System.out.println("Essa operação dá: " + (num1 - num2));
+	    }
+	}
+}
